31ab6b969c9fdc99c8f1e29eaa5716c8d563abf8
Keith McSpurren <info@track2development.com>
2018-12-14 15:59:08 -0500

    many changes to app

---
+++ my-app/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ActionCableProvider } from 'react-actioncable-provider';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { API_WS_ROOT } from './constants';
+
+ReactDOM.render(
+  <ActionCableProvider url={API_WS_ROOT}>
+    <App />
+    </ActionCableProvider>,
+    document.getElementById('root')
+    );
+    registerServiceWorker();
+
+
+/// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+
